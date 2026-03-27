e6c6e233abc0b0b08b1ec43b35f74b5a9e11110a
AjayAR <ajayperies10@gmail.com>
2026-03-27 22:55:37 +0530

    feat: Add initial UI, listing, booking, and admin dashboard features with related database migrations.

---
+++ Migrations/20260327135550_UpdateUserModel.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartBorrowLK.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Condition",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Listings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Listings",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Condition",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Listings");
+        }
+    }
+}
